a00ebf704a59ac0b6afe650721bb82618b1aafc6
devid <nnick4673@gmail.com>
2023-04-08 19:14:15 -0300

    Modelo de usuário implementado

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .forms import MyUserCreationForm, MyUserChangeForm
+from .models import MyUser
+
+
+class MyUserAdmin(UserAdmin):
+    add_form = MyUserCreationForm
+    form = MyUserChangeForm
+    model = MyUser
+    list_display = ('username', 'email', 'is_staff', 'is_active',)
+    list_filter = ('email', 'is_staff', 'is_active',)
+    fieldsets = (
+        (None, {'fields': ('username', 'email', 'password')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'groups', 'user_permissions')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'username', 'email', 'password1', 'password2', 'is_staff',
+                'is_active', 'groups', 'user_permissions'
+            )}
+        ),
+    )
+    search_fields = ('email',)
+    ordering = ('id',)
+
+
+admin.site.register(MyUser, MyUserAdmin)
+
--- users/models.py
+++ users/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, UserManager
+from django.utils import timezone
 
+class MyUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length = 255, unique = True)
+    username = models.CharField(max_length = 255)
+    is_active = models.BooleanField(default = True)
+    is_staff = models.BooleanField(default = False)
+    date_joined = models.DateTimeField(default = timezone.now)
+    objects = UserManager()
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
 
+    def __str__(self):
+        return f'User {self.id} - {self.email}'
+
+    class Meta:
+        verbose_name = 'user'
+        verbose_name_plural = 'users'
+        db_table = 'users'
+        ordering = ['id']
